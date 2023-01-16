ef471d20a8ff3b7d23ae06aa4e15fb8ca22ab74a
iangierhan <iangierhan@gmail.com>
2023-01-15 19:36:25 -0600

    Add project files.

---
+++ Migrations/20230116000129_RowVersion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace igierhan2749ex1a1.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                schema: "Application",
+                table: "People",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                schema: "Application",
+                table: "People");
+        }
+    }
+}
